915b0e239bb47108155ab586f611961e9e66379a
liaominghui <liaominghuikero@gmail.com>
2019-07-05 17:15:31 +0800

    fix

---
+++ springboot-hello/src/main/java/com.lmh.springboot.hello/limit/BUGFix.java
@@ -0,0 +1,16 @@
+package com.lmh.springboot.hello.limit;
+
+/**
+ * Copyright (C), 2019, 巴图鲁信息科技有限公司
+ * FileName: BUGFix
+ * Author:   liaominghuikero@gmail.com
+ * Date:     2019/7/5 17:15
+ * Description: TODO
+ * Version:1.0.0
+ */
+public class BUGFix {
+
+
+    Integer a = 1;
+
+}
